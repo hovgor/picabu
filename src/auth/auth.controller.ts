634bf19501f1d0278e90c9bab48bb4d60ea53f96
hovgor <gor95hov@gmail.com>
2022-09-08 11:24:17 +0400

    fix

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,7 +6,7 @@ import {
   Res,
   UnprocessableEntityException,
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Response } from 'express';
 import { EmailVerifyDto } from 'src/modules/users/dto/email.verify.dto';
 
@@ -24,6 +24,11 @@ export class AuthController {
   ) {}
 
   @Post('signUp/confirm')
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description:
+      'After verifying the email , you must log in to this mail to take a PIN code that lasts an hour after that the pin will not be valid . Required fields , name , smartphone ID , PIN code , email , password , password repetition , password must be at least 8 letters, of which at least one large letter and at least one digit.',
+  })
   async signUp(@Body() body: UserSignUpDto, @Res() res: Response) {
     try {
       const newUser = await this.authService.createNewUser(body);
@@ -34,6 +39,11 @@ export class AuthController {
   }
 
   @Post('signUp/verify')
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description:
+      'To register in the application, you need to pass email verification, after the request you will receive a PIN code to this email.',
+  })
   async signUpVerify(@Body() body: EmailVerifyDto, @Res() res: Response) {
     try {
       const existEmail = await this.usersService.getUserWhitEmail(body.email);
@@ -58,6 +68,10 @@ export class AuthController {
   }
 
   @Post('signIn')
+  @ApiResponse({
+    status: HttpStatus.ACCEPTED,
+    description: 'You need an email and password to login.',
+  })
   async userSignIn(@Body() body: UserSignInDto, @Res() res: Response) {
     try {
       const signIn = await this.authService.signInUser(body);
